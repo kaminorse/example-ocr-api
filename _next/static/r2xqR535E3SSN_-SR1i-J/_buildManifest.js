4e98c4671d431f6086aded8c7b9e4de1a813e8f0
Ryo Kamino <contact@kaminorse.com>
2023-06-27 16:44:41 +0900

    Updates

---
+++ _next/static/r2xqR535E3SSN_-SR1i-J/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/154-7942dd40332ec4bf.js","static/chunks/pages/index-486b11ffbc747b9f.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
